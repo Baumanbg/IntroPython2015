f08743160d632232fa32576a55f1e978eb199afe
Bruce Bauman <b.g.bauman@comcast.net>
2015-11-10 17:57:31 -0800

    monkey_trouble test routine

---
+++ students/Bruce/session_05/test_monkey_trouble.py
@@ -0,0 +1,16 @@
+
+#from codingbat import monkey_trouble
+from monkey_trouble import monkey_trouble
+
+def test_true_true():
+    assert monkey_trouble(True,True)
+    
+def test_false_false():
+    assert monkey_trouble(False,False)
+    
+def test_false_true():
+    assert monkey_trouble(False,True) is False
+    
+def test_true_false():
+    assert monkey_trouble(True,False) is False
+    
